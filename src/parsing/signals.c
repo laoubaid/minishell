080993475ae251b5b0615a3d5a32f92f392db15b
KAMAL EZ-ZOUBIR <kimonft@gmail.com>
2024-08-14 12:56:10 +0100

    feat: int signal working

--- src/parsing/signals.c
+++ src/parsing/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: laoubaid <laoubaid@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kez-zoub <kez-zoub@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 00:02:33 by kez-zoub          #+#    #+#             */
-/*   Updated: 2024/08/11 02:02:33 by laoubaid         ###   ########.fr       */
+/*   Updated: 2024/08/14 12:51:29 by kez-zoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ void	quit_shell(int signal)
 
 void	shell_signals(void)
 {
-	signal(SIGINT, quit_shell);
+	// signal(SIGINT, quit_shell);
+	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 }
